bd6d41a4364abe6532fc85ad7268dc4d900e3df0
CodeAPretzel <118945566+CodeAPretzel@users.noreply.github.com>
2023-10-01 20:09:30 -0500

    Update lsp_config.lua

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = { "sumneko_lua" }
+    ensure_installed = { "lua_ls" }
 })
 
 local on_attach = function(_, _)
@@ -9,13 +9,13 @@ local on_attach = function(_, _)
 
     vim.keymap.set('n', 'gd', vim.lsp.buf.definitionm {})
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-    vim.keymap.set('n', 'gr'. require('telescope.builtin').lsp_references, {})
+    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require("lspconfig").sumneko_lua.setup {
+require("lspconfig").lua_ls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
