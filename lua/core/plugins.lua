5b29cd649d559508c0932094d532cc952ad93008
CodeAPretzel <118945566+CodeAPretzel@users.noreply.github.com>
2023-10-01 19:58:50 -0500

    Update plugins.lua

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -24,7 +24,7 @@ return require('packer').startup(function(use)
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_lausnip'
+  use 'saadparwaiz1/cmp_luasnip'
   use 'rafamadriz/friendly-snippets'
 
   use {
