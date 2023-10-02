cb623afc3366fa8a1d2e9c7f8f5e52a6e98f4e3c
CodeAPretzel <pretzeldevro@gmail.com>
2023-10-01 19:07:51 -0500

    Commit - Revert
    Too Big of File Sizes
    -
    Unnecessary Features

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,7 +1,17 @@
--- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use spaces for tabs and whatnot
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', '"nohlsearch<CR>')
